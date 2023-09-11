513707ec353f24d227829adb5fff766acfc867f7
CJ <choijun0627@konkuk.ac.kr>
2023-09-11 22:48:59 +0900

    <feat> 스프링 DB 2편 프로젝트 section 10
    외부, 내부 트랜잭션 커밋
    
    내부 트랜잭션이 또 시작해서 커밋하는 경우 어떻게 되는지 설명하기 위한
    함수를 BasicTxTest에 추가함
    
    내부 트랜잭션은 커넥션을 얻거나 커밋해도 커넥션에 커밋하는 일이 일어나지
    않는다
    신규 트랜잭션이 아니면 실제 커넥션을 얻거나 커밋하지 않는다
    그래서 아무 일도 일어나지 않고 외부 트랜잭션에 참여하는 것이다
    그래서 외부 트랜잭션 커밋하면 그제서야 전체적으로 커밋되고 끝나는 것이다

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -71,4 +71,21 @@ public class BasicTxTest {
         txManager.rollback(tx2);
     }
 
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
+
 }
