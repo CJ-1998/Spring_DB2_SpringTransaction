55d75a97ca5c976550ee5a5de656b4028f27dc8f
CJ <choijun0627@konkuk.ac.kr>
2023-09-12 01:26:14 +0900

    <feat> 스프링 DB 2편 프로젝트 section 10
    트랜잭션 옵션 사용해서 외부, 내부 트랜잭션 따로 커밋, 롤백
    
    PROPATAION_REQUIRES_NEW 옵션을 트랜잭션에 사용하면 새로운 트랜잭션
    만들어서 새로운 커넥션 얻어낸다
    그래서 내부 트랜잭션에서 이렇게 하면 여기서 롤백해도 외부 트랜잭션에서는
    커밋할 수 있는 것이다
    이 옵션 사용하면 2개 트랜잭션을 분리해서 사용할 수 있는 것이다
    이것 확인하기 위해 BasicTxTest에 테스트 추가

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
@@ -122,5 +123,22 @@ public class BasicTxTest {
         assertThatThrownBy(() -> txManager.commit(outer)).isInstanceOf(UnexpectedRollbackException.class);
     }
 
+    @Test
+    void inner_rollback_requires_new() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
 
+        log.info("내부 트랜잭션 시작");
+        DefaultTransactionAttribute definition = new DefaultTransactionAttribute();
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
+        TransactionStatus inner = txManager.getTransaction(definition);
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+
+        log.info("내부 트랜잭션 롤백");
+        txManager.rollback(inner); //롤백
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer); //커밋
+    }
 }
