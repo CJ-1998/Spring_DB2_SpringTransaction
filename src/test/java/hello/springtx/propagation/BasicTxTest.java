1b203f28953333873cbbc818de4ec5c4cc4b72d3
CJ <choijun0627@konkuk.ac.kr>
2023-09-11 17:48:10 +0900

    <feat> 스프링 DB 2편 프로젝트 section 10
    트랜잭션 2개 사용
    
    트랜잭션 2개 사용해서 하면 커넥션 둘이 완전히 다른 것이라 영향이 없다는
    것을 보기 위해 BasicTxTest에 함수 추가

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -44,4 +44,31 @@ public class BasicTxTest {
         txManager.rollback(status);
         log.info("트랜잭션 롤백 완료");
     }
+
+    @Test
+    void double_commit() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 커밋");
+        txManager.commit(tx2);
+    }
+
+    @Test
+    void double_commit_rollback() {
+        log.info("트랜잭션1 시작");
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션1 커밋");
+        txManager.commit(tx1);
+
+        log.info("트랜잭션2 시작");
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("트랜잭션2 롤백");
+        txManager.rollback(tx2);
+    }
+
 }
