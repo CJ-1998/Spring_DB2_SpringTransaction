a61e63d45786c7c654ff69132a40655e1f289bfd
CJ <choijun0627@konkuk.ac.kr>
2023-09-12 16:50:36 +0900

    <feat> 스프링 DB 2편 프로젝트 section 11
    트랜잭션 전파 활용 예제
    
    이번에는 MemberService에서 예외를 복구하는 경우다.
    예외 복구하면 Member는 저장되고 Log만 롤백 되었을 것 같다.
    하지만 이전에 배운 내부 트랜잭션에서 롤백하면 rollback-only 마킹을 한다.
    그리고 memberService에서는 예외 복구해서 정상 흐름인 줄 알고 커밋하려고 하는데 rollback-only 있어서 롤백해야 한다.
    그래서 UnexpectedRollbackException까지 터트린다.
    그래서 Member, Log 둘 다 롤백된다.
    이것을 확인하기 위해 MemberServiceTest에 recoverException_fail 테스트 추가했다.
    그래서 이렇게 롤백 터져도 Member는 저장되고 Log는 롤백되는 방법을
    다음에 확인한다.

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.UnexpectedRollbackException;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -92,6 +94,22 @@ class MemberServiceTest {
         String username = "로그예외_outerTxOn_fail";
         //when
         assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+        //then: 모든 데이터가 롤백된다.
+        assertTrue(memberRepository.find(username).isEmpty());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void recoverException_fail() {
+        //given
+        String username = "로그예외_recoverException_fail";
+        //when
+        assertThatThrownBy(() -> memberService.joinV2(username)).isInstanceOf(UnexpectedRollbackException.class);
         //then: 모든 데이터가 롤백된다.
         assertTrue(memberRepository.find(username).isEmpty());
         assertTrue(logRepository.find(username).isEmpty());
