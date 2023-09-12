d50203d687842569612d5c08a9ea8ae8c90a4aa4
CJ <choijun0627@konkuk.ac.kr>
2023-09-12 15:11:29 +0900

    <feat> 스프링 DB 2편 프로젝트 section 11
    트랜잭션 전파 활용 예제
    
    이번에는 전부 @Transactional 있는 경우다.
    MemberService에서 MemberRepository, LogRepository 불러서 외부, 내부
    트랜잭션 개념 생긴다.
    그래서 이전에 배운 것처럼 전부 커밋하면 Repository의 트랜잭션들은 신규 트랜잭션이 아니라 커밋 요청하지만 실제 커밋이 되지 않고 MemberService에서 커밋 요청하면 이제 실제 커넥션에 커밋하는 것이다.
    이런 outerTxOn_success 테스트를 MemberServiceTest에 추가했다.

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -64,4 +64,20 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
     }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
+        //when
+        memberService.joinV1(username);
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
 }
