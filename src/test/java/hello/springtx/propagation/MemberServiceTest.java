b1f136b515567d1a238f233c41a68443a10b87f9
CJ <choijun0627@konkuk.ac.kr>
2023-09-12 14:21:03 +0900

    <feat> 스프링 DB 2편 프로젝트 section 11
    트랜잭션 전파 활용 예제
    
    한 트랜잭션은 커밋되고 한 트랜잭션은 롤백되는 상황
    각각 다른 트랜잭션이라 하나는 커밋되고 하나는 롤백된다
    그런데 Member와 Log가 다른 데이터 정합성 문제 생길 수 있다
    이런 것 보기 위한 outerTxOff_fail 테스트를 MemberServiceTest에 추가

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -32,4 +32,20 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isPresent());
     }
+
+    /**
+     * MemberService @Transactional:OFF
+     * MemberRepository @Transactional:ON
+     * LogRepository @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        String username = "로그예외_outerTxOff_fail";
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(username)).isInstanceOf(RuntimeException.class);
+        //then: 완전히 롤백되지 않고, member 데이터가 남아서 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isEmpty());
+    }
 }
