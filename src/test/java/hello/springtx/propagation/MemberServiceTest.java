3a021a2afdbd1167953b4952bc7bcb6aa2780c00
CJ <choijun0627@konkuk.ac.kr>
2023-09-12 14:57:24 +0900

    <feat> 스프링 DB 2편 프로젝트 section 11
    트랜잭션 전파 활용 예제
    
    이번에는 MemberService에만 @Transactional 있고 Repository 들에는 없는 것이다.
    그러면 하나의 트랜잭션으로 묶여서 같은 커넥션 사용하게 된다.
    그래서 MemberService에 @Transactional 추가하고 MemberRepository,
    LogRepository에는 뺀 것이다.
    그리고 이것을 확인하기 위해 MemberServiceTest에 singleTx라는 테스트
    추가했다.
    그런데 MemberService, MemberRepository, LogRepository 각각 트랜잭션
    사용하고 싶은 경우 생긴다.

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -48,4 +48,20 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
     }
+
+    /**
+     * MemberService @Transactional:ON
+     * MemberRepository @Transactional:OFF
+     * LogRepository @Transactional:OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "singleTx";
+        //when
+        memberService.joinV1(username);
+        //then: 모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
 }
